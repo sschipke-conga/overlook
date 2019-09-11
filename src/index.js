c812837058be323a84a946246dd9176bff90bab1
Scott Schipke <swschipke@gmail.com>
2019-09-11 16:21:12 -0600

    Add basic HTML, SCSS and jQuery for functioning tabs

--- src/index.js
+++ src/index.js
@@ -8,4 +8,17 @@ let roomsFetch = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms
 let bookingsFetch = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings');
 let roomServicesFetch = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/room-services/roomServices');
 
+//these control the tabs
+$('.tabs-stage div').hide();
+$('.tabs-stage div:first').show();
+$('.tabs-nav li:first').addClass('tab-active');
 
+// Change tab class and display content
+$('.tabs-nav a').on('click', function (event) {
+  event.preventDefault();
+  $('.tabs-nav li').removeClass('tab-active');
+  $(this).parent().addClass('tab-active');
+  $('.tabs-stage div').hide();
+  $($(this).attr('href')).show();
+});
+////
