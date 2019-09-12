d580eed0ece52ef16aa004bf4ec3e5f7472a0e0e
Scott Schipke <swschipke@gmail.com>
2019-09-11 18:51:55 -0600

    Instatiate hotel on page load

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import $ from 'jquery';
 import './css/base.scss';
 
 import './images/turing-logo.png'
+import Hotel from './Hotel';
 
-let usersFetch = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users').then(data => data.json()).then(data => console.log(data.users));
+let hotel;
+let usersFetch = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users').then(data => data.json()).then(data => makeHotel(data.users))
 let roomsFetch = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms').then(data => data.json()).then(data =>{
   let rooms = data.rooms;
 });
 let bookingsFetch = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings');
 let roomServicesFetch = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/room-services/roomServices');
+
+const makeHotel = (data) => {
+  hotel = new Hotel(data);
+  console.log(hotel)
+}
 
 //these control the tabs
 $('.tabs-stage div').hide();
